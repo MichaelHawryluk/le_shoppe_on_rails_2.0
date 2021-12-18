408dd59969de4d4ca32297e8cf09dd75a58368bf
mike <mike.mhawryluk@gmail.com>
2021-12-18 09:54:01 -0600

    products scaffold created, seed successful

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  validates :name, :description, :product_stock, :price, presence: true
 end
--- db/migrate/20211218152819_create_products.rb
+++ db/migrate/20211218152819_create_products.rb
@@ -3,8 +3,8 @@ class CreateProducts < ActiveRecord::Migration[6.1]
     create_table :products do |t|
       t.string :name
       t.string :description
-      t.int :price
-      t.int :product_stock
+      t.integer :price
+      t.integer :product_stock
 
       t.timestamps
     end
