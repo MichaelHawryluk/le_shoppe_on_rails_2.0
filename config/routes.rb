0ec159d85d5615a0d785e864011f0acb483976ec
mike <mike.mhawryluk@gmail.com>
2021-12-18 22:15:52 -0600

    products link in menu, home page rooted

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
   get '/search', to: 'search#index'
   resources :products
   resources :search
+  resources :abouts, only: %i[index new edit ]
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
